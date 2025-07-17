b93608d758156cea1534fa835d11f341c15bb9af
Bookkeeper <github.zhg2n@slmails.com>
2025-07-16 20:50:54 -0400

    Add files via upload

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { AppBar, Toolbar, Button } from '@mui/material';
-
 const navItems = [
   { path: '/', label: 'Dashboard' },
   { path: '/accounts', label: 'Accounts' },
@@ -13,28 +9,5 @@ const navItems = [
   { path: '/upgrade', label: 'Upgrade' },
   { path: '/breakup', label: 'Breakup Wizard' },
   { path: '/household', label: 'Household Manager' },
+  { path: '/income-sources', label: 'Income Sources' }, // <-- Add this line
 ];
-
-function Navigation() {
-  const { pathname } = useLocation();
-
-  return (
-    <AppBar position="static">
-      <Toolbar>
-        {navItems.map(item => (
-          <Button
-            key={item.path}
-            component={Link}
-            to={item.path}
-            color={pathname === item.path ? 'secondary' : 'inherit'}
-            sx={{ fontWeight: pathname === item.path ? 'bold' : 'normal', mx: 1 }}
-          >
-            {item.label}
-          </Button>
-        ))}
-      </Toolbar>
-    </AppBar>
-  );
-}
-
-export default Navigation;
