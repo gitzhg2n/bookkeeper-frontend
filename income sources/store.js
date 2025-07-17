f71329ae38dc9fae052287b430b0e167187c2a6a
Bookkeeper <github.zhg2n@slmails.com>
2025-07-16 20:36:59 -0400

    Add files via upload

---
+++ income sources/store.js
@@ -0,0 +1,17 @@
+import create from 'zustand';
+
+export const useStore = create(set => ({
+  accounts: [],
+  setAccounts: accounts => set({ accounts }),
+  budgets: [],
+  setBudgets: budgets => set({ budgets }),
+  transactions: [],
+  setTransactions: transactions => set({ transactions }),
+  householdMembers: [],
+  setHouseholdMembers: members => set({ householdMembers: members }),
+  investments: [],
+  setInvestments: investments => set({ investments }),
+  incomeSources: [],
+  setIncomeSources: sources => set({ incomeSources: sources }),
+  // Add more global state as needed
+}));
